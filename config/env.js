0e2c4f8b29ac993867eb6472f3d4c12c6e577310
nihal <nihalsheikh585@gmail.com>
2025-09-24 16:33:24 +0530

    ADDED: .env file config

---
+++ config/env.js
@@ -0,0 +1,6 @@
+import { config } from "dotenv";
+
+// config({ path: ".env" }); // use this for a single .env file
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const { PORT, NODE_ENV } = process.env;
