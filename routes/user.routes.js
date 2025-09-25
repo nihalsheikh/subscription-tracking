a27d53e05f297a0aae77e746a2c3aeb98a7795d1
nihal <nihalsheikh585@gmail.com>
2025-09-25 15:11:31 +0530

    ADDED: user controller to authorize user. MODIFIED: routes to use user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 // GET /users -> get all users
 // GET /users/:id -> get user by id
 
-userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
+userRouter.get("/", getUsers);
 
-userRouter.get("/:id", (req, res) => res.send({ title: "GET user details" }));
+userRouter.get("/:id", getUser);
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
 
