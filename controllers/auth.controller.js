7928aaa2c56ba9c201e60cde23a1836624ecd18f
nihal <nihalsheikh585@gmail.com>
2025-09-25 14:38:15 +0530

    MODIFIED: sign-in controller added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -56,6 +56,42 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
 	// Implement our Sign-up logic here
+	try {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			const error = new Error("User not found");
+			error.statusCode = 404;
+			throw error;
+		}
+
+		// compare the user entered password at sign-in with the one entered at sign-up
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			const error = new Error("Invalid password");
+			error.statusCode = 401;
+			throw error;
+		}
+
+		// generate new token at sign-in
+		const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+			expiresIn: JWT_EXPIRES_IN,
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "User signed in successfully",
+			data: {
+				token,
+				user,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 export const signOut = async (req, res, next) => {
