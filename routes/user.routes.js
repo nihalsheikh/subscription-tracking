af0cf1bb2878dd1aff4441970cfa9178efa5a2a4
nihal <nihalsheikh585@gmail.com>
2025-09-25 15:55:24 +0530

    ADDED: auth middleware to protect user routes access. MODIFIED: routes to add authorizaton

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -8,7 +9,7 @@ const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
 
